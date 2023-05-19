ff6afda499df703a731606b354259dc3dd5ce974
Zachary Piontek <zakpiontek@gmail.com>
2023-05-19 16:32:05 -0700

    added post route for create note and function -working-

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from './Notes.module.css';
+import CreateNote from "./CreateNote";
 
 async function getNotes() {
     const response = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
@@ -17,6 +18,7 @@ export default async function NotesPage() {
                     return <Note key={note.id} note={note} />;
                 })}
             </div>
+            <CreateNote />
         </main>
     )
 }
